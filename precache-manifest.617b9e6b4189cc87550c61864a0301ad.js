dea744d9718b0ac93a94a61e941b47bf2e68d503
Ahmed Abosaif <ahmed.t.abosaif@gmail.com>
2021-06-12 05:29:57 +0200

    first commit

---
+++ precache-manifest.617b9e6b4189cc87550c61864a0301ad.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd967e2d34d857c2ca81d404a2f396c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c28cabfb0dd11541125e",
+    "url": "/static/css/2.e39f83b6.chunk.css"
+  },
+  {
+    "revision": "4791fed3738ac85c3ccd",
+    "url": "/static/css/main.7fa725a3.chunk.css"
+  },
+  {
+    "revision": "c28cabfb0dd11541125e",
+    "url": "/static/js/2.bc65178b.chunk.js"
+  },
+  {
+    "revision": "846523b9f31e4513b60a69a3b0d13874",
+    "url": "/static/js/2.bc65178b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4791fed3738ac85c3ccd",
+    "url": "/static/js/main.8665a3a1.chunk.js"
+  },
+  {
+    "revision": "6e0680e49eaca0aff96187570bc4ffc9",
+    "url": "/static/js/main.8665a3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3dcb7a1b76c5b792c1",
+    "url": "/static/js/runtime-main.8bb26cf8.js"
+  },
+  {
+    "revision": "20b238d454fe547904a42e1044cff1b0",
+    "url": "/static/media/App.20b238d4.png"
+  },
+  {
+    "revision": "169cde85802ea430ae2a4a8eb363a0c7",
+    "url": "/static/media/Computer.169cde85.png"
+  },
+  {
+    "revision": "6a037b687197db0740515dbc4989bb35",
+    "url": "/static/media/File.6a037b68.png"
+  },
+  {
+    "revision": "5aceaa069a2fb0ba365501f3e51a269a",
+    "url": "/static/media/Mail.5aceaa06.png"
+  },
+  {
+    "revision": "bce60030d4b369949971f883a75095c1",
+    "url": "/static/media/Network.bce60030.png"
+  },
+  {
+    "revision": "02d82fb4f94018c5202ad77c54a812d2",
+    "url": "/static/media/Printer.02d82fb4.png"
+  },
+  {
+    "revision": "db87164209f5d92030237757ed3f1650",
+    "url": "/static/media/Sap.db871642.png"
+  },
+  {
+    "revision": "7d6ad242c2c21041a920f289cc74d2ac",
+    "url": "/static/media/SelfServicePortalBanne1r.7d6ad242.jpg"
+  },
+  {
+    "revision": "1ad2c1e60ae70cebb3607a930df5cfe1",
+    "url": "/static/media/V3.1ad2c1e6.pdf"
+  },
+  {
+    "revision": "0a3ba1143b67851e4920023ec16ebfdf",
+    "url": "/static/media/bg.0a3ba114.jpg"
+  },
+  {
+    "revision": "2cfc05f64d299579ea80e7057a512f0d",
+    "url": "/static/media/bkg-gry.2cfc05f6.jpg"
+  },
+  {
+    "revision": "53a658e5769b54fa9cd19bd2d16e1198",
+    "url": "/static/media/clipart-camera-security-camera-7-transparent.53a658e5.png"
+  },
+  {
+    "revision": "8e74c56aaf681406d9121b67676b44d8",
+    "url": "/static/media/globe.8e74c56a.jpg"
+  },
+  {
+    "revision": "9ad52af83bc15294d3d4e49880a63c4e",
+    "url": "/static/media/iP-Cam.9ad52af8.png"
+  },
+  {
+    "revision": "a3591d531ad4ae8591b2bfce15d90fe3",
+    "url": "/static/media/iP-Phone.a3591d53.png"
+  },
+  {
+    "revision": "0a842203182a22896da13096982b6e28",
+    "url": "/static/media/it-ticketing-system.0a842203.png"
+  },
+  {
+    "revision": "f78c172683426057db5a93cdd9363ba3",
+    "url": "/static/media/itsm-software.f78c1726.png"
+  },
+  {
+    "revision": "5dd4f6f6cd967a8b540167cf11262ece",
+    "url": "/static/media/logo_egyption_steel.5dd4f6f6.png"
+  },
+  {
+    "revision": "b6a8fad62a16761d73f442c4c3da0504",
+    "url": "/static/media/map2.b6a8fad6.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "0a3ba1143b67851e4920023ec16ebfdf",
+    "url": "/static/media/s1.0a3ba114.jpg"
+  }
+]);
